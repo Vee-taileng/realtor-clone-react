af96fbe23812b4a05fffb1c480a93d636bc0ea03
Vee_Sengwan <virapongsengwan@gmail.com>
2023-03-21 21:23:51 +0700

    Complete the sign up page using google authentication

--- src/component/OAuth.jsx
+++ src/component/OAuth.jsx
@@ -1,9 +1,40 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const OAuth = () => {
+  const navigate = useNavigate()
+
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider);
+      const user = result;
+
+      // Check for the user
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp()
+        })
+      }
+      navigate("/")
+    } catch (error) {
+      toast.error("Could not sign up with Google! Please recheck!")
+    }
+  }
   return (
-      <button className="w-full bg-red-700 
+    <button type="button"
+    onClick={onGoogleClick}
+    className="w-full bg-red-700 
     flex justify-center items-center
     text-white px-7 py-3 uppercase text-sm font-medium
     hover:bg-red-800
