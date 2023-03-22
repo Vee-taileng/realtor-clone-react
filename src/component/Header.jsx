4ef1867b35bb4ebc5e66194ab9425087a3c5a0af
Vee_Sengwan <virapongsengwan@gmail.com>
2023-03-22 22:31:34 +0700

    Complete the header component - when the user is active, the sign in tab change to profile tab

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 const Header = () => {
-    const urlLocation = useLocation()
+  const [pageState, setPageState] = useState("Sign in")
+  const urlLocation = useLocation()
   const navigate = useNavigate()
-  
+  const auth = getAuth()
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile")
+      } else {
+        setPageState("Sign in")
+      }
+    })
+  }, [auth])
     function pathMatchRoute(route) {
         return route === urlLocation.pathname
     }
@@ -29,8 +40,10 @@ const Header = () => {
                          ${pathMatchRoute("/offers") && "text-black border-b-[3px] border-b-red-500"}`}
                         onClick={()=> navigate("/offers")}>Offers</li>
                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400
-                         ${pathMatchRoute("/sign-in") && "text-black border-b-[3px] border-b-red-500"}`}
-                        onClick={()=> navigate("/sign-in")}>Sign in</li>
+                         ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "text-black border-b-[3px] border-b-red-500"}`}
+                        onClick={() => navigate("/profile")}>
+                          {pageState}
+                        </li>
                   </ul>
               </div>
         </header>
